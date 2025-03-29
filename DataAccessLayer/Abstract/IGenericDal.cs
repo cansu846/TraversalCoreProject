10796bad21a81dc17d0ac356b261b53dd1047f55
Cansu Kocaoglu <cansukocaoglu55@gmail.com>
2025-03-29 19:15:28 +0300

    feat: created entities dal

---
+++ DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T>
+    {
+        public void Insert(T t);
+        public void Delete(T t);
+        public void Update(T t);
+        public List<T> GetList();
+
+
+
+    }
+}
