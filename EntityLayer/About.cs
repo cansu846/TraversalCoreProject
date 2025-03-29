7f9c32455fb6965819fa568042886767a6bac54f
Cansu Kocaoglu <cansukocaoglu55@gmail.com>
2025-03-29 17:27:30 +0300

    feat: added new entity class

--- EntityLayer/About.cs
+++ EntityLayer/About.cs
@@ -9,8 +9,11 @@ namespace EntityLayer
     public class About
     {
         public int AboutID{ get; set; }
-        public string Image { get; set; }
-        public string Details { get; set; }
+        public string Title { get; set; }
+        public string Image1 { get; set; }
+        public string Description1 { get; set; }
+        public string Image2 { get; set; }
+        public string Description2 { get; set; }
         public bool Status { get; set; }
     }
 }
