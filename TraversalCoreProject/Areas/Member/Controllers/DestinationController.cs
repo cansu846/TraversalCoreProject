150b6506703292911325f72b6fce8c8e10c6de10
Cansu Kocaoglu <cansukocaoglu55@gmail.com>
2025-04-10 14:56:43 +0300

    feat: sign up işlemlerinin yapılması

---
+++ TraversalCoreProject/Areas/Member/Controllers/DestinationController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Abstract;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Areas.Member.Controllers
+{
+    [Area("Member")]
+    [AllowAnonymous]
+    public class DestinationController : Controller
+    {
+        IDestinationService destinationService = new DestinationManager(new EfDestinationDal());
+        public IActionResult Index()
+        {
+            var values = destinationService.TGetList();
+            return View(values);
+        }
+    }
+}
