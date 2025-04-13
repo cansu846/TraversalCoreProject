ccd1e918b3b08338356b4a0fd0ee8296df697688
Cansu Kocaoglu <cansukocaoglu55@gmail.com>
2025-04-13 20:14:47 +0300

    feat: added guide operations

---
+++ BusinessLayer/Concrete/GuideManager.cs
@@ -0,0 +1,44 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class GuideManager : IGuideService
+    {
+        private readonly IGuideDal _GuideDal;   
+        public GuideManager(IGuideDal GuideDal)
+        {
+            _GuideDal = GuideDal;
+        }
+        public void TAdd(Guide t)
+        {
+            _GuideDal.Insert(t);
+        }
+
+        public void TDelete(Guide t)
+        {
+            _GuideDal.Delete(t);
+        }
+
+        public Guide TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Guide> TGetList()
+        {
+           return _GuideDal.GetList();
+        }
+
+        public void TUpdate(Guide t)
+        {
+            _GuideDal.Update(t);    
+        }
+    }
+}
---
+++ TraversalCoreProject/Areas/ViewComponents/MemberDashboard/_GuideList.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Abstract;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Areas.ViewComponents.MemberDashboard
+{
+    public class _GuideList:ViewComponent
+    {
+        private readonly IGuideService _guideManager = new GuideManager(new EfGuideDal());
+        public IViewComponentResult Invoke()
+        {
+            var guides = _guideManager.TGetList();
+            return View(guides);  
+        }
+    }
+}
