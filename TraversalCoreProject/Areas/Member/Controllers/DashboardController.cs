5b75cf936ac1d4f94b0c5b93ea3ce9930a7ee523
Cansu Kocaoglu <cansukocaoglu55@gmail.com>
2025-04-13 16:26:36 +0300

    feat: added dashboard index page

---
+++ TraversalCoreProject/Areas/Member/Controllers/DashboardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Areas.Member.Controllers
+{
+    [Area("Member")]
+    [Route("Member/[controller]/[action]")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
