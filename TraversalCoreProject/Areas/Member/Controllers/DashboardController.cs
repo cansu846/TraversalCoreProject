ca2d6d07f39c95cdb2b38e82277fd8250bdf8021
Cansu Kocaoglu <cansukocaoglu55@gmail.com>
2025-04-13 16:33:07 +0300

    feat: created dashboard profile side

--- TraversalCoreProject/Areas/Member/Controllers/DashboardController.cs
+++ TraversalCoreProject/Areas/Member/Controllers/DashboardController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace TraversalCoreProject.Areas.Member.Controllers
 {
@@ -6,8 +9,18 @@ namespace TraversalCoreProject.Areas.Member.Controllers
     [Route("Member/[controller]/[action]")]
     public class DashboardController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<AppUser> _userManager;
+
+        public DashboardController(UserManager<AppUser> userManager)
         {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.userNameSurname = user.UserName + " " + user.Surname;
+            ViewBag.userImage = user.ImageUrl;
             return View();
         }
     }
