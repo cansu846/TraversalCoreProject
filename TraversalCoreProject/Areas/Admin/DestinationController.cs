bd58457ad629674762260fa70431d41bed63e20d
Cansu Kocaoglu <cansukocaoglu55@gmail.com>
2025-04-16 21:52:16 +0300

    feat: added admin destination list

---
+++ TraversalCoreProject/Areas/Admin/DestinationController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Abstract;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Areas.Admin
+{
+    [Area("Admin")]
+    [Route("/Admin/[controller]/[action]")]
+    [AllowAnonymous]
+    public class DestinationController : Controller
+    {
+        private readonly IDestinationService destinationService = new DestinationManager(new EfDestinationDal());
+        public IActionResult Index()
+        {
+            var values = destinationService.TGetList();
+            return View(values);
+        }
+    }
+}
---
+++ TraversalCoreProject/Areas/Member/ViewComponents/MemberDashboard/_ProfileInformation.cs
@@ -0,0 +1,26 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProject.Areas.Member.ViewComponents.MemberDashboard
+{
+    public class _ProfileInformation:ViewComponent
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public _ProfileInformation(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.memberFullName = user.Name +" "+ user.Surname;
+            ViewBag.memberPhone = user.PhoneNumber;
+            ViewBag.memberEmail = user.Email;
+            return View();  
+        }
+    }
+}
