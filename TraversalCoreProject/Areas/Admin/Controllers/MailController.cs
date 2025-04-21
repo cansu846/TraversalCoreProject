6271eb3857e4ff16a74cb1295465c09e1ac401e5
Cansu Kocaoglu <cansukocaoglu55@gmail.com>
2025-04-22 00:15:05 +0300

    feat: added admin send message service

---
+++ TraversalCoreProject/Areas/Admin/Controllers/MailController.cs
@@ -0,0 +1,48 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using TraversalCoreProject.Models;
+
+namespace TraversalCoreProject.Areas.Admin.Controllers
+{
+
+    [Area("Admin")]
+    [Route("Admin/[controller]/[action]")]
+    [AllowAnonymous]
+    public class MailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(MailRequest mailRequest)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+
+            MailboxAddress mailboxAddress = new MailboxAddress("Admin", "cansukocaoglu55@gmail.com");
+            mimeMessage.From.Add(mailboxAddress);
+
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User", mailRequest.ReceiverMail);
+            mimeMessage.To.Add(mailboxAddressTo);
+
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = mailRequest.Body;
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+
+            mimeMessage.Subject = mailRequest.Subject;
+
+            SmtpClient client = new SmtpClient();
+            client.Connect("smtp.gmail.com",587, false);
+            client.Authenticate("cansukocaoglu55@gmail.com", "kcpobjxkfcussvnk");
+            client.Send(mimeMessage);
+            client.Disconnect(true);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
