0a054d819b535314ac643683740d1194e530983d
Cansu Kocaoglu <cansukocaoglu55@gmail.com>
2025-04-22 23:32:47 +0300

    feat: start using ajax

---
+++ TraversalCoreProject/Areas/Admin/Controllers/CityUsingAjaxController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using TraversalCoreProject.Areas.Admin.Models;
+
+namespace TraversalCoreProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/[controller]/[action]")]
+    [AllowAnonymous]
+    public class CityUsingAjaxController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CityList() {
+            //Json() metodu otomatik olarak cities listesini JSON'a çevirir. 
+            return Json(cities);
+        }
+
+        List<CityViewModel> cities = new List<CityViewModel>
+        {
+            new CityViewModel
+            {
+                CityId=1,
+                CityName="Paris",
+                CityCountry="France"
+            },
+            new CityViewModel
+            {
+                CityId=2,
+                CityName="Paris",
+                CityCountry="France"
+            },
+            new CityViewModel
+            {
+                CityId=3,
+                CityName="Paris",
+                CityCountry="France"
+            },
+            new CityViewModel
+            {
+                CityId=4,
+                CityName="Paris",
+                CityCountry="France"
+            }
+        };
+    }
+}
---
+++ TraversalCoreProject/Areas/Admin/Models/CityViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TraversalCoreProject.Areas.Admin.Models
+{
+    public class CityViewModel
+    {
+        public int CityId { get; set; }
+        public string CityName { get; set; }
+        public string CityCountry { get; set; }
+    }
+}
