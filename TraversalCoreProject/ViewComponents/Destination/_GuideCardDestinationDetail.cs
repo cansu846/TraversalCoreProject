55d49dec23c3f908988ebeed03a38790e5551842
Cansu Kocaoglu <cansukocaoglu55@gmail.com>
2025-05-19 20:44:27 +0300

    refactor: deatinationdetrail view page edited

---
+++ TraversalCoreProject/ViewComponents/Destination/_GuideCardDestinationDetail.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace TraversalCoreProject.ViewComponents.Destination
+{
+    public class _GuideCardDestinationDetail:ViewComponent
+    {
+        private readonly IGuideService _guideService;
+        Random rnd;
+        public _GuideCardDestinationDetail(IGuideService guideService)
+        {
+            _guideService = guideService;
+            rnd = new Random();
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var randomNumber = rnd.Next(1, _guideService.TGetList().Count);
+            var value = _guideService.TGetById(randomNumber);
+            ViewBag.number = randomNumber;
+            return View(value);
+        }
+    }
+}
